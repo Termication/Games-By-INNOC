835765a4b309a92b373102f381a8a60e3a2858c7
Terminal <terminalkarabo@gmail.com>
2024-02-03 16:32:05 +0200

    testing programs

---
+++ raylib test/raylib testing/main.cpp
@@ -0,0 +1,44 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define BALL_RADIUS 20
+#define BALL_SPEED 5
+
+int main() {
+    // Initialization
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "KayBee Bouncing Ball");
+    SetTargetFPS(60);
+
+    // Ball properties
+    Vector2 ballPosition = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2};
+    Vector2 ballSpeed = {BALL_SPEED, BALL_SPEED};
+
+    while (!WindowShouldClose()) {
+        // Update
+        ballPosition.x += ballSpeed.x;
+        ballPosition.y += ballSpeed.y;
+
+        // Bounce off the walls
+        if ((ballPosition.x + BALL_RADIUS) >= SCREEN_WIDTH || (ballPosition.x - BALL_RADIUS) <= 0) {
+            ballSpeed.x *= -1;
+        }
+        if ((ballPosition.y + BALL_RADIUS) >= SCREEN_HEIGHT || (ballPosition.y - BALL_RADIUS) <= 0) {
+            ballSpeed.y *= -1;
+        }
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Draw the ball
+        DrawCircleV(ballPosition, BALL_RADIUS, RED);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
+}
---
+++ test/raylib/main.c
@@ -0,0 +1,45 @@
+#include "raylib.h"
+#include <math.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+#define BALL_RADIUS 20
+#define BALL_SPEED 5
+
+int main(void) {
+    // Initialization
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bouncing Ball");
+    SetTargetFPS(60);
+
+    // Ball properties
+    Vector2 ballPosition = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2};
+    Vector2 ballSpeed = {BALL_SPEED, BALL_SPEED};
+
+    while (!WindowShouldClose()) {
+        // Update
+        ballPosition.x += ballSpeed.x;
+        ballPosition.y += ballSpeed.y;
+
+        // Bounce off the walls
+        if ((ballPosition.x + BALL_RADIUS) >= SCREEN_WIDTH || (ballPosition.x - BALL_RADIUS) <= 0) {
+            ballSpeed.x *= -1;
+        }
+        if ((ballPosition.y + BALL_RADIUS) >= SCREEN_HEIGHT || (ballPosition.y - BALL_RADIUS) <= 0) {
+            ballSpeed.y *= -1;
+        }
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Draw the ball
+        DrawCircleV(ballPosition, BALL_RADIUS, RED);
+
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
+
+    return 0;
+}
