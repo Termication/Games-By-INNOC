5d21a4fae380770aa2e71aafd82730b2089b978f
Terminal <terminalkarabo@gmail.com>
2024-02-17 18:28:57 +0200

    game window created

--- tetris/Building Tetris/main.cpp
+++ tetris/Building Tetris/main.cpp
@@ -6,4 +6,12 @@ using namespace std;
 int main()
 {
     InitWindow(300, 600, "Tetris By INN.");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
 }
