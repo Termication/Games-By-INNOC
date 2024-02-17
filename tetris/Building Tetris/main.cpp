e1a58f48bf5bee3d9f788cf99ed290463638279b
Terminal <terminalkarabo@gmail.com>
2024-02-17 18:32:18 +0200

    background color added

--- tetris/Building Tetris/main.cpp
+++ tetris/Building Tetris/main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main()
 {
+    Color darkBlue = {44, 44, 127, 255};
     InitWindow(300, 600, "Tetris By INN.");
     SetTargetFPS(60);
 
     while(WindowShouldClose() == false){
         BeginDrawing();
+        ClearBackground(darkBlue);
 
         EndDrawing();
     }
