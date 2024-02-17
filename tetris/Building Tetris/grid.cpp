d49bbc3287ee0a0b5f69df1742d2c341acca6f7e
Terminal <terminalkarabo@gmail.com>
2024-02-17 18:52:05 +0200

    testing the print cout method

--- tetris/Building Tetris/grid.cpp
+++ tetris/Building Tetris/grid.cpp
@@ -1,2 +1,27 @@
 
 #include "grid.h"
+
+Grid::Grid()
+{
+    numRows = 20;
+    numCols = 10;
+    cellSize = 30;
+    Initialize();
+}
+
+void Grid::Initialize()
+{
+    for(int row = 0; row < numRows; row++){
+        for(int column; column < numCols; column++){
+            grid[row][column] = 0;
+        }
+    }
+}
+void Grid::Print(){
+    for(int row = 0; row < numRows; row++){
+        for(int column = 0; column < numCols; column++){
+        std::cout << grid[row][column] << " ";
+        }
+        std::cout << std::endl();
+    }
+}
