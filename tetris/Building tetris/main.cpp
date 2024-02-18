6f51b8786475adb685dd466fa1afd029c78fa918
Terminal <terminalkarabo@gmail.com>
2024-02-18 13:54:38 +0200

    game lop and creating the Grid  done successfully

--- tetris/Building tetris/main.cpp
+++ tetris/Building tetris/main.cpp
@@ -11,6 +11,9 @@ int main()
     SetTargetFPS(60);
 
     Grid grid; // Create a Grid object
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
     grid.Print(); // Just for testing purposes
 
     while (!WindowShouldClose()) {
