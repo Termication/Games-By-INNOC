cdd676979b5bdbe8ad406b44d6aa61366a3b09fb
Terminal <terminalkarabo@gmail.com>
2024-02-18 16:07:43 +0200

    method for getting random block

--- tetris/Building tetris/game.cpp
+++ tetris/Building tetris/game.cpp
@@ -1,1 +1,22 @@
+#include "game.h"
 
+
+Game::Game(){
+
+    grid = Grid();
+    blocks = {IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()};
+}
+
+Block Game::GetRandomBlock(){
+    if(blocks.empty()){
+
+    }
+    int randomIndex = rand() % blocks.size();
+    Block block = blocks[randomIndex];
+    blocks.erase(blocks.begin() + randomIndex);
+    return block;
+}
+
+std::vector<Block> Game::GetAllBlock(){
+
+}
--- tetris/Building tetris/game.h
+++ tetris/Building tetris/game.h
@@ -1,1 +1,14 @@
+#pragma once
+#include "grid.h"
+#include "block.cpp"
 
+class Game{
+public:
+    Game();
+    Block GetRandomBlock();
+    std::vector<Block> GetAllBlock();
+    Grid grid;
+
+private:
+    std::vector<Block> blocks;
+};
