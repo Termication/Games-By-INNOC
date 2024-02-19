dc1cf6deaac9fc061a0efbffcd9727216de9d982
Terminal <terminalkarabo@gmail.com>
2024-02-19 16:06:46 +0200

    adding the user interface

--- tetris/Building tetris/grid.cpp
+++ tetris/Building tetris/grid.cpp
@@ -33,7 +33,7 @@ void Grid::Draw(){
     for(int row = 0; row < numRows; row++){
         for(int column = 0; column < numCols; column++){
             int cellValue = grid[row][column];
-            DrawRectangle(column * cellSize+1, row * cellSize+1, cellSize-1, cellSize-1, colors[cellValue]);
+            DrawRectangle(column * cellSize+11, row * cellSize+11, cellSize-1, cellSize-1, colors[cellValue]);
 
         }
     }
--- tetris/Building tetris/main.cpp
+++ tetris/Building tetris/main.cpp
@@ -18,7 +18,7 @@ bool EventTriggered(double interval){
 int main()
 {
     Color darkBlue = {44, 44, 127, 255};
-    InitWindow(300, 600, "Tetris By INN.");
+    InitWindow(500, 620, "Tetris By INN.");
     SetTargetFPS(60);
 
     Game game = Game();
