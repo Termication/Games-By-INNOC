8de2ecf5d6e988886667cef98a6e742086ed622f
Terminal <terminalkarabo@gmail.com>
2024-02-19 17:39:48 +0200

    blocks are centered on rectangle using switch statement

--- tetris/Building tetris/game.cpp
+++ tetris/Building tetris/game.cpp
@@ -27,7 +27,21 @@ std::vector<Block> Game::GetAllBlocks() {
 void Game::Draw(){
     grid.Draw();
     currentBlock.Draw(11, 11);
-    nextBlock.Draw(270, 270);
+
+    switch(nextBlock.id){
+        case 3:
+            nextBlock.Draw(255, 290);
+            break;
+
+        case 4:
+            nextBlock.Draw(255, 280);
+            break;
+
+        default:
+            nextBlock.Draw(270, 270);
+            break;
+
+    }
 }
 
 void Game::HandleInput(){
