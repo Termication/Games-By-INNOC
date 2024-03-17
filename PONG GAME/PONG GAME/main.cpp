302f410534279e7026ab49172fca4d7423ccc44d
Terminal <terminalkarabo@gmail.com>
2024-03-17 13:45:00 +0200

    the ball can move now

--- PONG GAME/PONG GAME/main.cpp
+++ PONG GAME/PONG GAME/main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+class Ball{
+  public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update(){
+    x += speed_x;
+    y += speed_y;
+    }
+};
+
+Ball ball;
+
 int main()
 {
     const int screen_width = 1280;
@@ -10,13 +28,21 @@ int main()
     InitWindow(screen_width, screen_height, "Pong Game by INN");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while (WindowShouldClose() == false)
     {
          BeginDrawing();
+         ball.Update();
+         ClearBackground(BLACK);
 
          // drawing
          DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-         DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+         ball.Draw();
          DrawRectangle(10,screen_height/ 2 - 60,25,120, WHITE);
          DrawRectangle(screen_width - 35,screen_height/ 2 - 60,25,120, WHITE);
 
