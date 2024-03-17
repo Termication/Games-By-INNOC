a1e34ebf037e027f3157ad9f9aa7e32fd0aaf36f
Terminal <terminalkarabo@gmail.com>
2024-03-17 13:34:06 +0200

    paddles and the the ball drawn

--- PONG GAME/PONG GAME/main.cpp
+++ PONG GAME/PONG GAME/main.cpp
@@ -6,13 +6,19 @@ using namespace std;
 int main()
 {
     const int screen_width = 1280;
-    const int screen_height = 800;
+    const int screen_height = 600;
     InitWindow(screen_width, screen_height, "Pong Game by INN");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false)
     {
          BeginDrawing();
+
+         // drawing
+         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
+         DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+         DrawRectangle(10,screen_height/ 2 - 60,25,120, WHITE);
+         DrawRectangle(screen_width - 35,screen_height/ 2 - 60,25,120, WHITE);
 
 
          EndDrawing();
