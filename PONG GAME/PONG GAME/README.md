34d982afea82787835339be7f5147cf3e486c76b
Terminal <terminalkarabo@gmail.com>
2024-03-17 13:21:54 +0200

    window created

--- PONG GAME/PONG GAME/README.md
+++ PONG GAME/PONG GAME/README.md
@@ -1,4 +1,4 @@
 
-making the old PONG GAME, using raylib,
+coding the old PONG GAME from scratch, using raylib,
 Guided by Youtube
 cred: Nick
--- PONG GAME/PONG GAME/main.cpp
+++ PONG GAME/PONG GAME/main.cpp
@@ -7,7 +7,16 @@ int main()
 {
     const int screen_width = 1280;
     const int screen_height = 800;
-    InitWindow(screen_width, screen_height "Pong Game by INN");
+    InitWindow(screen_width, screen_height, "Pong Game by INN");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+         BeginDrawing();
+
+
+         EndDrawing();
+    }
 
     CloseWindow();
 
