e2c364f4b1e220450edceda12a14f861be5b9fb9
Terminal <terminalkarabo@gmail.com>
2024-05-23 16:12:33 +0200

    spaceship prevented from moving out of the window

--- SPACE INVADERS/spaceship.cpp
+++ SPACE INVADERS/spaceship.cpp
@@ -20,8 +20,16 @@ void Spaceship::Draw()
 
 void Spaceship::MoveLeft(){
     position.x -= 7;
+    //prevent movement out of the window
+    if(position.x < 0){
+        position.x = 0;
+    }
 }
 
 void Spaceship::MoveRight(){
     position.x += 7;
+    //prevent movement out of the window
+    if(position.x > GetScreenWidth() - image.width){
+        position.x = GetScreenWidth() - image.width;
+    }
 }
