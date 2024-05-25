c75f997106719f303806aed11408df7f8387b046
Terminal <terminalkarabo@gmail.com>
2024-05-25 10:21:52 +0200

    block implementation

--- SPACE INVADERS/block.cpp
+++ SPACE INVADERS/block.cpp
@@ -1,1 +1,10 @@
+#include "block.hpp"
 
+Block::Block(Vector2 position){
+    this -> position = position;
+
+}
+// implcementation to draw a block
+void Block::Draw(){
+    DrawRectangle(position.x, position.y, 3, 3, {243, 216, 63, 255});
+}
