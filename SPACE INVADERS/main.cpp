3b4a003b244602967510bf8c3e3715f910ce0a16
Terminal <terminalkarabo@gmail.com>
2024-05-31 23:05:18 +0200

    commented

--- SPACE INVADERS/main.cpp
+++ SPACE INVADERS/main.cpp
@@ -1,32 +1,24 @@
 #include <iostream>
 #include <raylib.h>
-#include "game.hpp"
-#include "fire.hpp"
-
 
 using namespace std;
 
 int main()
 {
+    //background color
     Color grey = {29, 29, 27, 255};
     int windowWidth = 750;
     int windowHeight = 700;
 
+    //Window declare
     InitWindow(windowWidth, windowHeight, "Space Invaders by INN");
     SetTargetFPS(60);
 
-    Game game;
-
     while(WindowShouldClose() == false){
-
-        game.HandleInput();
-        game.Update();
 
         BeginDrawing();
         ClearBackground(grey);
-
-        game.Draw();
-
+        
         EndDrawing();
     }
 
